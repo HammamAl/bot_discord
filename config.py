9b3fd3e572f46525ec99e574b7c3f56e125af657
HammamAl <alfarisihammam@gmail.com>
2025-01-01 16:23:22 +0700

    commit pertama

--- config.py
+++ config.py
@@ -5,17 +5,17 @@ from dotenv import load_dotenv
 # --- Discord Config ---
 load_dotenv()
 DISCORD_TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
-CHANNEL_ID = 1320978354417041491  # ID saluran Discord tempat bot mengirim notifikasi
-CHANNEL_NAME = "general"  # Nama saluran Discord (opsional, untuk validasi)
+CHANNEL_ID: Final[int] = int(os.getenv("CHANNEL_ID"))  # ID saluran Discord tempat bot mengirim notifikasi
+CHANNEL_NAME: Final[str] = os.getenv("CHANNEL_NAME")  # Nama saluran Discord (opsional, untuk validasi)
 
 # ESP32 Config
 AMONIA_AMBANG_BATAS = 30  # Ambang batas Amonia untuk memicu notifikasi otomatis
 
 # MQTT Config
-MQTT_BROKER = "499b37cd93464a848333539b957a57ef.s1.eu.hivemq.cloud"
-MQTT_PORT = 8883  # Port untuk koneksi TLS
-MQTT_USER = "percobaan1"
-MQTT_PASSWORD = "Percobaan2024"
+MQTT_BROKER: Final[str] = os.getenv("MQTT_BROKER")
+MQTT_PORT: Final[int] = int(os.getenv("MQTT_PORT")) # Port untuk koneksi TLS
+MQTT_USER: Final[str] = os.getenv("MQTT_USER")
+MQTT_PASSWORD: Final[str] = os.getenv("MQTT_PASSWORD")
 MQTT_TOPIC_RELAY_STATUS = "relay/notifications"  # Topik untuk pemberitahuan relay
 MQTT_RELAY_CONTROL_TOPIC = "esp32/relay"  # Topik kontrol relay
 MQTT_SENSOR_DATA_TOPIC =  "sensor/data" # Topik data sensor
